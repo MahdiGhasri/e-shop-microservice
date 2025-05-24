6b3cf478ca81f10edc7c2b39c931f2163d54fc31
Mohammad Mahdi Ghasri <43605741+MahdiGhasri@users.noreply.github.com>
2025-05-24 14:53:51 +0200

    added keyclock image

---
+++ services/order-service/src/main/java/com/mgh/order/configuration/RestClientConfiguration.java
@@ -0,0 +1,55 @@
+package com.mgh.order.configuration;
+
+// import java.time.Duration;
+
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+// import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.client.ClientHttpRequestFactory;
+// import org.springframework.web.client.RestClient;
+// import org.springframework.web.client.support.RestClientAdapter;
+// import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+// import com.mgh.order.customer.CustomerClient;
+// import com.mgh.order.payment.PaymentClient;
+
+// @Configuration
+public class RestClientConfiguration {
+
+    // @Value("${application.config.customer-url}")
+    // private String customerServiceUrl;
+    
+    // @Value("${application.config.payment-url}")
+    // private String paymentServiceUrl;
+    
+	// @Bean
+    // CustomerClient customerClient() {
+    //     RestClient restClient = RestClient.builder()
+    //             .baseUrl(customerServiceUrl)
+    //             .requestFactory(getClientRequestFactory())
+    //             .build();
+    //     var restClientAdapter = RestClientAdapter.create(restClient);
+    //     var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+    //     return httpServiceProxyFactory.createClient(CustomerClient.class);
+    // }
+	
+	// @Bean
+    // PaymentClient paymentClient() {
+    //     RestClient restClient = RestClient.builder()
+    //             .baseUrl(paymentServiceUrl)
+    //             .requestFactory(getClientRequestFactory())
+    //             .build();
+    //     var restClientAdapter = RestClientAdapter.create(restClient);
+    //     var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+    //     return httpServiceProxyFactory.createClient(PaymentClient.class);
+    // }
+
+    // private ClientHttpRequestFactory getClientRequestFactory() {
+    //     ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.defaults()
+    //             .withConnectTimeout(Duration.ofSeconds(3))
+    //             .withReadTimeout(Duration.ofSeconds(3));
+    //     return ClientHttpRequestFactoryBuilder.simple().build(clientHttpRequestFactorySettings);
+    // }
+}
