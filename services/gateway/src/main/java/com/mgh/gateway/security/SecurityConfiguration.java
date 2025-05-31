335b537e0987b55d3bf71996f9961afadd51aea2
Mehdi Ghasri <mahdighasri@gmail.com>
2025-05-31 11:00:05 +0200

    tried for using keycloack oath2

--- services/gateway/src/main/java/com/mgh/gateway/security/SecurityConfiguration.java
+++ services/gateway/src/main/java/com/mgh/gateway/security/SecurityConfiguration.java
@@ -1,39 +1,46 @@
 package com.mgh.gateway.security;
 
-// import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity; 
+import org.springframework.security.config.web.server.ServerHttpSecurity; 
+import org.springframework.security.web.server.SecurityWebFilterChain; 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource; 
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource; 
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.CorsConfigurationSource;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
-// @Configuration
+//@Configuration
+//@EnableWebFluxSecurity 
 public class SecurityConfiguration {
 
-	// private final String[] freeResourceUrls = { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-	// 		"/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus", "/eureka/**" };
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
 
-	// @Bean
-	// SecurityFilterChain springSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-	// 	return httpSecurity
-	// 			.authorizeHttpRequests(authorize -> authorize.requestMatchers(freeResourceUrls).permitAll().anyRequest()
-	// 					.authenticated())
-	// 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-	// 			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) 
+                                                            
+                .authorizeExchange(authorize -> authorize 
+                        .pathMatchers(freeResourceUrls)
+                        .permitAll()
+                        .anyExchange().authenticated() 
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) 
+                .build();
+    }
 
-	// }
-
-	// @Bean
-	// CorsConfigurationSource corsConfigurationSource() {
-	// 	CorsConfiguration configuration = new CorsConfiguration();
-	// 	configuration.applyPermitDefaultValues();
-	// 	configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-	// 	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	// 	source.registerCorsConfiguration("/**", configuration);
-	// 	return source;
-	// }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
